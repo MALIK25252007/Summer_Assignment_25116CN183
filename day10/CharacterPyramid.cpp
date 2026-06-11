b2750733b6eab26be7198befee1b2b3ded57cf61
Tarun Malik <malik1642007@gmail.com>
2026-06-11 14:13:38 +0530

    Update CharacterPyramid.cpp

--- day10/CharacterPyramid.cpp
+++ day10/CharacterPyramid.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
     int n;
     cout << "Enter number of rows: ";
     cin >> n;
@@ -15,5 +15,6 @@ int main() {
 
         cout << endl;
     }
+    
     return 0;
 }
